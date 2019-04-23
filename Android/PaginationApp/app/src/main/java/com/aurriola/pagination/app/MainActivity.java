16afd69fd124e519835fbe8b403283fe46f09398
Alexander Urriola <alexx198906@gmail.com>
2019-04-22 21:20:25 -0500

    1. agrego vista para BottomNavigation

---
+++ Android/PaginationApp/app/src/main/java/com/aurriola/pagination/app/MainActivity.java
@@ -0,0 +1,45 @@
+package com.aurriola.pagination.app;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+//                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_search:
+//                    mTextMessage.setText(R.string.title_explore);
+                    return true;
+                case R.id.navigation_queue:
+//                    mTextMessage.setText(R.string.title_queue);
+                    return true;
+                case R.id.navigation_profile:
+//                    mTextMessage.setText(R.string.title_profile);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
